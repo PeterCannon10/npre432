b886b6060a9fd952326536049c7ae0ccf6d7260b
Peter Cannon <peterac2@illinois.edu>
2026-09-20 18:41:11 -0500

    deliverable 2 almost done, but find_properties broken due to array dimensions

--- lab-2/data_processing.py
+++ lab-2/data_processing.py
@@ -10,7 +10,21 @@ COLORS = ["#E63946", "#F4A261", "#2A9D8F", "#457B9D", "#7B2CBF", "#264653"]
 def csv_to_array(csv_filename, skip_rows):
     return pd.read_csv(os.path.join(DATA_DIR, csv_filename), skiprows=skip_rows).to_numpy()
 
+def find_properties(stress, strain, initial_gage_diameter, final_gage_diameter):
+    elastic_modulus = stress/strain
+    yield_strength = stress[np.where(strain >= 0.002)[0][0]]
+    ultimate_strength = np.max(stress)
+    elongation_percent = 100 * ((final_gage_diameter-initial_gage_diameter)/initial_gage_diameter)
+    resilience_modulus = yield_strength**2/(2*elastic_modulus)
+    return np.array([elastic_modulus, yield_strength, ultimate_strength, elongation_percent, resilience_modulus])
+
 def main():
+    #[304SS, 1018CR, 1045NM, 2024, BR, PMMA]
+    initial_gage_diameters = np.array([7.14, 7.14, 7, 7.15, 7.08, 7.99]) #diameters in mm
+    grip_diameters = np.array([12.68, 12.70, 12.58, 12.68, 12.58, 12.74])
+    rockwell_hardness = np.array([99.8, 93.8, 92.2, 73.2, 67.1, 0]) #unconverted
+    final_gage_diameters = np.array([3.57, 5.3, 5.52, 6.01, 6.03, 7.4])
+
     dataset = []
     file_names = []
     for csv in os.listdir(DATA_DIR):
@@ -32,6 +46,7 @@ def main():
     for material in file_names:
         time, displacement, force, strain = dataset[i]
         area = 1 #TEMP FIX THIS
+        stress = force/area
         
         material_label = material[4:-6]
         print(material_label)
@@ -40,22 +55,28 @@ def main():
             plt.close()
 
             plt.figure()
-            plt.plot(strain, force/area, color=COLORS[-1])
+            plt.plot(strain, stress, color=COLORS[-1])
             plt.xlabel("Strain")
             plt.ylabel("Stress (N) currently -- SHOULD BE STRESS (MPa)")
             plt.grid()
             plt.savefig(os.path.join(IMAGES_DIR, 'pmma_stress_strain_plot.png'))
             plt.close()
+            break
         if material_label == "1018": #change label to 304SS
             material_label = "304SS"
-            
-        plt.plot(strain, force/area, color=COLORS[i], label=material_label)
+
+        plt.plot(strain, stress, color=COLORS[i], label=material_label)
         plt.xlabel("Strain")
         plt.ylabel("Stress (N) currently -- SHOULD BE STRESS (MPa)")
         plt.grid()
         plt.legend()
         i+=1
 
+    """
+    Deliverable 2
+    """
+    for j in range(len(file_names)):
+        print(find_properties(stress, strain, initial_gage_diameters[i], final_gage_diameters[i]))
     
     
 
