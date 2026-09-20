07839980876d2226605dccd441db85f3eba8cf75
Peter Cannon <peterac2@illinois.edu>
2026-09-20 17:39:49 -0500

    deliverable 1 complete -- need to correct area based on grip or gage d

--- lab-2/data_processing.py
+++ lab-2/data_processing.py
@@ -3,28 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-DATA_DIR = './raw_data'
-IMAGES_DIR = './images'
+DATA_DIR = 'raw_data'
+IMAGES_DIR = 'images'
 COLORS = ["#E63946", "#F4A261", "#2A9D8F", "#457B9D", "#7B2CBF", "#264653"]
 
 def csv_to_array(csv_filename, skip_rows):
     return pd.read_csv(os.path.join(DATA_DIR, csv_filename), skiprows=skip_rows).to_numpy()
 
-def plot_stress_strain_curve(time, displacement, force, composite_strain, material_name):
-    plt.figure(figsize=(10, 6))
-    plt.plot(composite_strain, force, color=COLORS[0], label='Stress-Strain Curve')
-    plt.title(f'Stress-Strain Curve for {material_name}')
-    plt.xlabel('Composite Strain')
-    plt.ylabel('Force (N) currently -- SHOULD BE STRESS (MPa)')
-    plt.grid()
-    plt.legend()
-    plt.savefig(os.path.join(IMAGES_DIR, f'{material_name}_stress_strain_curve.png'))
-    plt.close()
-
-
 def main():
-    file_names = []
     dataset = []
+    file_names = []
     for csv in os.listdir(DATA_DIR):
         if csv.endswith('.csv'):
             file_names.append(csv)
@@ -33,7 +21,44 @@ def main():
                 skip_rows = 31
             else:
                 skip_rows = 29
-            data_array = csv_to_array(csv, skip_rows)
+        data_array = csv_to_array(csv, skip_rows).T
+        dataset.append(data_array)
+
+    """
+    Deliverable 1
+    """
+    i = 0
+    plt.figure()
+    for material in file_names:
+        time, displacement, force, strain = dataset[i]
+        area = 1 #TEMP FIX THIS
+        
+        material_label = material[4:-6]
+        print(material_label)
+        if material_label == "PMMA":
+            plt.savefig(os.path.join(IMAGES_DIR, 'stress_strain_curves.png'))
+            plt.close()
+
+            plt.figure()
+            plt.plot(strain, force/area, color=COLORS[-1])
+            plt.xlabel("Strain")
+            plt.ylabel("Stress (N) currently -- SHOULD BE STRESS (MPa)")
+            plt.grid()
+            plt.savefig(os.path.join(IMAGES_DIR, 'pmma_stress_strain_plot.png'))
+            plt.close()
+        if material_label == "1018": #change label to 304SS
+            material_label = "304SS"
+            
+        plt.plot(strain, force/area, color=COLORS[i], label=material_label)
+        plt.xlabel("Strain")
+        plt.ylabel("Stress (N) currently -- SHOULD BE STRESS (MPa)")
+        plt.grid()
+        plt.legend()
+        i+=1
+
+    
+    
+
 
 
     
